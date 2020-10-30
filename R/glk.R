3c98b07cee354ea60c8d4cac9af29115df1ac12a
RonaldVisser <38214052+RonaldVisser@users.noreply.github.com>
2020-10-30 21:42:10 +0100

    Included probability of exceedence
    Included probability of exceedence and named items in resulting list

--- R/glk.R
+++ R/glk.R
@@ -1,13 +1,20 @@
 # glk.R from dplR (https://cran.r-project.org/src/contrib/dplR_1.6.7.tar.gz) has been used as inspiration
 # calulate Gleichlaufigkeit
 # author of this version: Ronald Visser based on versions by Christian Zang, Mikko Korpela and Allan Buras
-glk <- function(x, overlap = 50) {
-  if(!("rwl" %in% class(x))) warning("'x' is not class rwl")
+glk <- function(x, overlap = 50, prob = TRUE) {
+  if(!("rwl" %in% class(x))) {
+    warning("'x' is not class rwl")
+    }
   if(any(length(overlap)!=1 | !is.numeric(overlap) | 
          overlap%%1!=0 | overlap < 3)){
     stop("'overlap' should be a single integer >=3")
-  }
-  if(overlap < 50) warning("The minimum number of overlap is lower than 50. This might lead to statistically insignificant matches.")
+    }
+  if(overlap < 50) {
+    warning("The minimum number of overlap is lower than 50. This might lead to statistically insignificant matches.")
+    }
+  if (!is.logical(prob)) {
+    stop("'prob' must be either TRUE (the default) or FALSE")
+    }
   # function starts here
   n <- dim(x)[2]
   GLK_mat <- matrix(NA_real_, nrow = n, ncol = n)
@@ -26,5 +33,17 @@ glk <- function(x, overlap = 50) {
     GLK_mat[i,] <- GLK_values
     Overlap_n[i,] <- treering_GCol
   }
-  glk <- list(GLK_mat,Overlap_n)
+  diag(GLK_mat) <- 1
+  if (prob == TRUE) {
+    s_df <- 1 /(2*sqrt(Overlap_n))
+    z_df <- (GLK_mat-0.5)/s_df
+    z_normcdf <- apply(z_df, 2, function(z) pnorm(z, mean=0, sd=1))
+    GLK_p <- 2*(1-z_normcdf)   
+    glk_list <- list(glk_mat = GLK_mat,
+                overlap = Overlap_n,
+                p_mat = GLK_p)
+  } else {
+    glk_list <- list(glk_mat = GLK_mat,
+                overlap = Overlap_n)}
+  glk_list
 }
--- R/sgc.R
+++ R/sgc.R
@@ -3,23 +3,30 @@
 # Published as: Visser, R.M., 2020: On the similarity of tree‐ring patterns: Assessing the influence of semi‐synchronous growth changes on the Gleichläufigkeitskoeffizient for big tree‐ring data sets, Archaeometry 2020.
 # DOI: https://doi.org/10.1111/arcm.12600
 # SGC = Synchronous Growth Changes
-# to extract list of SGC/SSGC use command below
-# SSGC <- sgc_mat[lower.tri(sgc_mat)]
-# SGC <- sgc_mat[upper.tri(sgc_mat)]
 
-sgc <- function(x, overlap = 50) {
+
+sgc <- function(x, overlap = 50, prob = TRUE) {
   # checks class rwl and correct overlap
-  if(!("rwl" %in% class(x))) warning("'x' is not class rwl")
+  if(!("rwl" %in% class(x))) {
+    warning("'x' is not class rwl")
+    }
   if(any(length(overlap)!=1 | !is.numeric(overlap) | 
          overlap%%1!=0 | overlap < 3)){
     stop("'overlap' should be a single integer >=3")
-  }
-  if(overlap < 50) warning("The minimum number of overlap is lower than 50. This might lead to statistically insignificant matches.")
+    }
+  if(overlap < 50) {
+    warning("The minimum number of overlap is lower than 50. This might lead to statistically insignificant matches.")
+    }
+  if (!is.logical(prob)) {
+    stop("'prob' must be either TRUE (the default) or FALSE")
+    }
   # function starts here
   n <- dim(x)[2]
   sgc_mat <- matrix(NA_real_, nrow = n, ncol = n)
+  ssgc_mat <- matrix(NA_real_, nrow = n, ncol = n)
   overlap_n <- matrix(NA_real_, nrow = n, ncol = n)
   rownames(sgc_mat) <- colnames(sgc_mat) <- names(x)
+  rownames(ssgc_mat) <- colnames(ssgc_mat) <- names(x)
   rownames(overlap_n) <- colnames(overlap_n) <- names(x)
   treering_sign <- apply(x, 2, diff)
   treering_sign <- sign(treering_sign)
@@ -35,14 +42,23 @@ sgc <- function(x, overlap = 50) {
     treering_GC0 <- colSums(treering_GC==0,na.rm=TRUE)
     SGC_values <- treering_GC0 / treering_GCol
     SSGC_values <- treering_GC1 / treering_GCol
-    sgc_mat[i,-seq(1:i)] <- SGC_values[-seq(1:i)]
-    # transpose and place SSGC in the lower triangle of the SGC-matrix
-    sgc_mat[-seq(1:i),i] <- SSGC_values[-seq(1:i)]
-    overlap_n[i,-seq(1:i)] <- treering_GCol[-seq(1:i)]
+    sgc_mat[i,] <- SGC_values
+    ssgc_mat[i,] <- SSGC_values
+    overlap_n[i,] <- treering_GCol
   }
-  # mirror overlap to both triangles of the matrix
-  overlap_n[lower.tri(overlap_n)] <- overlap_n[upper.tri(overlap_n)]
-  # overlap of self comparisons is length of series - 1
-  #diag(overlap_n) <- colSums(!is.na(x))-1
-  sgc <- list(sgc_mat,overlap_n)
-}
+  if (prob == TRUE) {
+    s_mat <- 1 /(2*sqrt(overlap_n))
+    z_mat <- (sgc_mat-0.5)/s_mat
+    z_normcdf <- apply(z_mat, 2, function(z) pnorm(z, mean=0, sd=1))
+    p_mat <- 2*(1-z_normcdf) 
+    sgc_list <- list(sgc_mat = sgc_mat,
+                     ssgc_mat = ssgc_mat,
+                     overlap = overlap_n,
+                     p_mat = p_mat)
+  } else {
+    sgc_list <- list(sgc = sgc_mat,
+                     ssgc = ssgc_mat,
+                     overlap = overlap_n)
+  }
+  sgc_list
+  }
